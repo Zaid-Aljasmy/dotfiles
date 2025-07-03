52c3e8007f8a4979fce085c06069a62db96449f7
Zaid-Aljasmy <ttrmux@proton.me>
2025-07-03 19:01:14 +0300

    update neovim

--- .config/nvim/lua/plugins/zen-mode.lua
+++ .config/nvim/lua/plugins/zen-mode.lua
@@ -1,8 +1,15 @@
 return {
-    {
-  "folke/zen-mode.nvim",
-  config = function()
-    require("zen-mode").setup({
-    })
-  end
-}}
+  {
+    "folke/zen-mode.nvim",
+    config = function()
+      require("zen-mode").setup({
+        window = {
+          width = function()
+            return vim.api.nvim_win_get_width(0)
+          end,
+        },
+      })
+    end
+  }
+}
+
