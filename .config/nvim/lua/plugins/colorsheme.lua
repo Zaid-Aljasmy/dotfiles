bdfc8aee333fc78afb192c0085bd7550ce092243
Zaid-Aljasmy <ttrmux@proton.me>
2025-07-10 18:10:27 +0300

    new color

--- .config/nvim/lua/plugins/colorsheme.lua
+++ .config/nvim/lua/plugins/colorsheme.lua
@@ -1,12 +1,12 @@
 return {
   {
-    "folke/tokyonight.nvim",
+    "craftzdog/solarized-osaka.nvim",
     lazy = false,
     priority = 1000,
     transparent = false,
     terminal_colors = true,
     config = function()
-    vim.cmd[[colorscheme tokyonight-night]]
+    vim.cmd[[colorscheme solarized-osaka]]
     -- vim.api.nvim_set_hl(0, "Visual", { bg = "#504945", fg = "NONE" }) -- enable this only for gruvbox theme
     end,
   },
