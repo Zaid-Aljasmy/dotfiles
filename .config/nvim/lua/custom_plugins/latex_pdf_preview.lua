e576b68f5fda3b7b447c54d0ab94b41c91330553
Zaid <ttrmux@proton.me>
2025-11-13 19:50:02 +0300

    Update My Custom LaTeX-Preview Plugin

--- .config/nvim/lua/custom_plugins/latex_pdf_preview.lua
+++ .config/nvim/lua/custom_plugins/latex_pdf_preview.lua
@@ -1,14 +1,36 @@
 local M = {}
-M.zathura_opened = false 
+M.zathura_opened = false
+
+local home = vim.fn.expand("~")
+M.source_dir = home .. "/LaTeX/LaTeX-Source-Files"
+M.main_dir = home .. "/LaTeX/LaTeX-PDF"
+M.build_dir = M.main_dir .. "/build"
+M.pdf_dir = M.main_dir .. "/pdf"
 
 M.build_and_view = function()
   local texfile = vim.fn.expand("%:p")
-  local pdffile = texfile:gsub("%.tex$", ".pdf")
+  local filename = vim.fn.expand("%:t:r")
 
-  vim.fn.system("pdflatex -interaction=nonstopmode -halt-on-error " .. texfile)
+  vim.fn.mkdir(M.source_dir, "p")
+  vim.fn.mkdir(M.build_dir, "p")
+  vim.fn.mkdir(M.pdf_dir, "p")
+
+  local build_pdf = M.build_dir .. "/" .. filename .. ".pdf"
+  local final_pdf = M.pdf_dir .. "/" .. filename .. ".pdf"
+
+  local cmd = string.format(
+    "pdflatex -interaction=nonstopmode -halt-on-error -output-directory=%s %s",
+    M.build_dir,
+    texfile
+  )
+  vim.fn.system(cmd)
+
+  if vim.fn.filereadable(build_pdf) == 1 then
+    vim.fn.system(string.format("mv -f %s %s", build_pdf, final_pdf))
+  end
 
   if not M.zathura_opened then
-    vim.fn.jobstart("zathura " .. pdffile, {detach = true})
+    vim.fn.jobstart("zathura " .. final_pdf, { detach = true })
     M.zathura_opened = true
   end
 end
@@ -18,7 +40,7 @@ M.enable_auto_build = function()
     pattern = "*.tex",
     callback = function()
       M.build_and_view()
-    end
+    end,
   })
 end
 
