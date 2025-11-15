65fbf517055b887a1f26a07eeea3098a0c1050cf
Zaid <ttrmux@proton.me>
2025-11-15 07:46:35 +0300

    Remove old nvim configuration directory

--- .config/nvim/init.lua
+++
@@ -1,7 +0,0 @@
--- Load core config and initialize lazy.nvim plugin manager
-
-require("custom_plugins.latex_pdf_preview") -- My custom plugin for LaTeX preview
-require("config.keybinds")
-require("config.lazy")
-require("config.settings")
-require("lazy").setup("plugins")
