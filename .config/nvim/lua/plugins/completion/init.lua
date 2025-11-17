e2c8689a6544dca3f78c686a221af851d873b33c
Zaid <ttrmux@proton.me>
2025-11-17 05:52:06 +0300

    r

--- .config/nvim/lua/plugins/completion/init.lua
+++
@@ -1,9 +0,0 @@
-return {
-    require("plugins.completion.MathKeys"),
-    require("plugins.completion.cmp"),
-    require("plugins.completion.codeium"),
-    require("plugins.completion.autopairs"),
-    require("plugins.completion.gitsigns"),
-    require("plugins.completion.highlight-colors"),
-}
-
--- .config/nvim/lua/plugins/tools/init.lua
+++
@@ -1,5 +0,0 @@
-return {
-    require("plugins.tools.comment"),
-    require("plugins.tools.debug"),
-    require("plugins.tools.toggleterm"),
-}
