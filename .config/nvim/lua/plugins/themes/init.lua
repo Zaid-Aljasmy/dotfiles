3b2be2207276b3959cad72d8a2886a44b3b24c6c
Zaid <ttrmux@proton.me>
2025-12-05 03:21:27 +0300

    remove old dirs & add new dirs

--- .config/nvim/lua/plugins/themes/init.lua
+++
@@ -1,7 +0,0 @@
-return {
-    require("plugins.themes.tokyonight"),
-    -- require("plugins.themes.catppuccin"),
-    -- require("plugins.themes.moonfly"),
-    -- require("plugins.themes.everforest"),
-    -- require("plugins.themes.tokyodark"),
-}
--- .config/nvim/lua/plugins/ui/indentline.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "lukas-reineke/indent-blankline.nvim",
-  main = "ibl",
-  opts = {
-    indent = { char = "│" }, -- you can use -│- or -┃- 
-    scope = { enabled = false }, -- make this true to use treesitter scope
-  },
-}
-
